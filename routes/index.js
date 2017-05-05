0ec98c732b8220fe1159406a4f8766ee3247481f
Shashank Srivastava <shashank@Shashanks-MacBook-Air.local>
2017-05-05 16:40:50 +0530

    submit

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,16 @@ var router = express.Router();
 
 var userController = require('../controllers/user');
 
-router.route('/v1/users')
+router.route('/users')
   .post(userController.postUsers)
   .get(userController.getUsers);
 
-router.route('/v1/users/update/:id')
+router.route('/users/update/:id')
 	.put(userController.updateUsers)
 	.delete(userController.deleteUsers) 
   .get(userController.idsearch);
 
-router.route('/v1/users/search/:reg')
+router.route('/users/search/:reg')
   .get(userController.regexsearch);
 
 
